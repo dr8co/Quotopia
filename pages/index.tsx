fd316ce9ef65ed99306696df6813cca750d7a4be
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-09-19 18:06:21 +0300

    Add the footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
@@ -6,6 +7,8 @@ import styles from '@/styles/Home.module.css'
 import {
     BackgroundImage1,
     BackgroundImage2,
+    FooterCon,
+    FooterLink,
     GradientBackgroundCon
 } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
@@ -14,6 +17,7 @@ import Clouds1 from '../assets/cloud-and-thunder.png'
 import Clouds2 from '../assets/cloudy-weather.png'
 
 export default function Home() {
+    const [numQuotes, setumNuQotes] = useState<number | null>(0);
     return (
         <>
             <Head>
@@ -37,6 +41,19 @@ export default function Home() {
                     height="300"
                     alt="cloud2"
                 />
+
+                {/* Footer */}
+                <FooterCon>
+                    <>
+                        Quotes Generated: {numQuotes}
+                        <br/>
+                        Developed with 💛 by&nbsp;
+                        <FooterLink href="https://github.com/dr8co" target="_blank" rel="noopener noreferrer">
+                            <b>Ian Duncan</b>
+                        </FooterLink>
+                    </>
+                </FooterCon>
+
             </GradientBackgroundCon>
 
         </>
