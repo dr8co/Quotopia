a39eb10c14ebdecb70ad0fe12c9d4d8089be9223
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-09-19 16:22:55 +0300

    Clean up the template

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: false,
+    compiler: {
+        styledComponents: {
+            ssr: true,
+            displayName: true,
+            topLevelImportPaths: [],
+            meaninglessFileNames: ["index"],
+            cssProp: true,
+            namespace: "",
+            fileName: true,
+            minify: false,
+            transpileTemplateLiterals: false,
+            pure: false
+        }
+    }
 }
 
 module.exports = nextConfig
