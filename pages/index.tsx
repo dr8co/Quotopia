ced309f9b45939995721167924ff8bb10ef3984d
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-09-19 16:41:00 +0300

    Add a gradient background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import {GradientBackgroundCon} from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Quotopia: Inspirational Quote Generator</title>
-        <meta name="description" content="A simple" />
+        <meta name="description" content="A simple inspirational quote generator" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+        {/* Background */}
+        <GradientBackgroundCon>
+
+        </GradientBackgroundCon>
+
     </>
   )
 }
